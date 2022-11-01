ab3968b2b10c805c3dd5d48a321c6fbf9f98331f
kocety91 <kocety91@gmail.com>
2022-11-01 16:27:09 +0200

    Add Service Collection Extensions

---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,100 @@
+﻿using BookWebAPI.Data;
+using BookWebAPI.Filters;
+using BookWebAPI.Models;
+using BookWebAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BookWebAPI.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureDataBase(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<BookDbContext>
+            (opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            service.AddDefaultIdentity<ApplicationUser>(opt =>
+             {
+                 opt.Password.RequireDigit = false;
+                 opt.Password.RequireLowercase = false;
+                 opt.Password.RequireUppercase = false;
+                 opt.Password.RequiredLength = 3;
+                 opt.Password.RequireNonAlphanumeric = false;
+             })
+                  .AddRoles<ApplicationRole>()
+                  .AddEntityFrameworkStores<BookDbContext>();
+
+            return service;
+        }
+
+        public static IServiceCollection ConfigureJwt(this IServiceCollection service, IConfiguration configuration)
+        {
+            var key = Encoding.UTF8.GetBytes(configuration["Jwt:Key"]);
+            var tokenValidationParamaters = new TokenValidationParameters()
+            {
+                ValidateActor = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                ClockSkew = TimeSpan.Zero,
+                IssuerSigningKey = new SymmetricSecurityKey(key)
+            };
+
+            service.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opt =>
+            {
+                opt.SaveToken = true;
+                opt.RequireHttpsMetadata = false;
+                opt.TokenValidationParameters = tokenValidationParamaters;
+                opt.Events = new JwtBearerEvents
+                {
+                    OnChallenge = async context =>
+                    {
+                        context.HandleResponse();
+
+                        context.Response.StatusCode = 401;
+                        context.Response.Headers.Append("Unauthorized", "User");
+                        context.Response.ContentType = "application/json";
+
+                        var errorResponse = new ErrorDetails
+                        {
+                            Message = "You are not authorized !",
+                            Error = context.Response.StatusCode,
+                        };
+
+                        await context.Response.WriteAsync(errorResponse.ToString());
+                    },
+                };
+            });
+
+            service.AddSingleton(tokenValidationParamaters);
+            return service;
+        }
+
+        public static IServiceCollection ConfigureBusinesServices(this IServiceCollection service,IConfiguration configuration)
+        {
+            service.AddTransient<IBookService, BookService>();
+            service.AddTransient<IPublisherService, PublisherService>();
+            service.AddTransient<IGenreService, GenreService>();
+            service.AddTransient<IAuthorService, AuthorService>();
+            service.AddTransient<IIdentityService, IdentityService>();
+            service.AddTransient<ExceptionHandlingMiddleware>();
+            service.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            return service;
+        }
+
+        public static IServiceCollection ConfigureFilters(this IServiceCollection service,IConfiguration configuration)
+        {
+            service.AddControllers(opt => opt.Filters.Add(new BookActionFilter()));
+            return service;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,108 +1,31 @@
 using BookWebAPI.Data;
 using BookWebAPI.Extensions;
-using BookWebAPI.Filters;
-using BookWebAPI.Models;
 using BookWebAPI.Seeding;
-using BookWebAPI.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var configuration = builder.Configuration;
-builder.Services.AddDbContext<BookDbContext>
-    (opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
-
-builder.Services.AddDefaultIdentity<ApplicationUser>(opt =>
-{
-    opt.Password.RequireDigit = false;
-    opt.Password.RequireLowercase = false;
-    opt.Password.RequireUppercase = false;
-    opt.Password.RequiredLength = 3;
-    opt.Password.RequireNonAlphanumeric = false;
-})
-    .AddRoles<ApplicationRole>()
-    .AddEntityFrameworkStores<BookDbContext>();
-
-
-var key = Encoding.UTF8.GetBytes(configuration["Jwt:Key"]);
-var tokenValidationParamaters = new TokenValidationParameters()
-{
-    ValidateActor = true,
-    ValidateIssuer = false,
-    ValidateAudience = false,
-    ValidateIssuerSigningKey =true,
-    ClockSkew = TimeSpan.Zero,
-    IssuerSigningKey = new SymmetricSecurityKey(key)
-};
-
-builder.Services.AddAuthentication(opt =>
-{
-    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(opt =>
-  {
-      opt.SaveToken = true;
-      opt.RequireHttpsMetadata = false;
-      opt.TokenValidationParameters = tokenValidationParamaters;
-      opt.Events = new JwtBearerEvents
-      {
-          OnChallenge = async context =>
-          {
-              context.HandleResponse();
-
-              context.Response.StatusCode = 401;
-              context.Response.Headers.Append("Unauthorized", "User");
-              context.Response.ContentType = "application/json";
-
-              var errorResponse = new ErrorDetails
-              {
-                  Message = "You are not authorized !",
-                  Error = context.Response.StatusCode,
-              };
-
-              await context.Response.WriteAsync(errorResponse.ToString());
-          },
-      };
-  });
-
-
-builder.Services.AddTransient<IBookService, BookService>();
-builder.Services.AddTransient<IPublisherService, PublisherService>();
-builder.Services.AddTransient<IGenreService, GenreService>();
-builder.Services.AddTransient<IAuthorService, AuthorService>();
-builder.Services.AddTransient<IIdentityService, IdentityService>();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddControllers(opt => opt.Filters.Add(new BookActionFilter()));
-builder.Services.AddTransient<ExceptionHandlingMiddleware>();
-builder.Services.AddSingleton(tokenValidationParamaters);
-
+builder.Services
+    .ConfigureDataBase(configuration)
+    .ConfigureJwt(configuration)
+    .ConfigureBusinesServices(configuration)
+    .ConfigureFilters(configuration)
+    .AddAuthorization()
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen();
 
 
 var app = builder.Build();
 
-//app.UseHttpLogging();
 
-//seed data
-using(var serviceScope = app.Services.CreateScope())
+using (var serviceScope = app.Services.CreateScope())
 {
     var dbContext = serviceScope.ServiceProvider.GetRequiredService<BookDbContext>();
     dbContext.Database.Migrate();
-    new BookDbContextSeeder().SeedAsync(dbContext,serviceScope.ServiceProvider).GetAwaiter().GetResult();
+    new BookDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
 }
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
