d4e32e4c54f21b508f355a671785cb9c70ea75c0
kocety91 <kocety91@gmail.com>
2022-11-04 16:15:46 +0200

    Add BookService Tests

---
+++ Tests/Services/AuthorServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using BookWebAPI.Data;
+using BookWebAPI.Models;
+using BookWebAPI.Repositories;
+using BookWebAPI.Services;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace BookWebAPI.Tests.Services
+{
+    public class AuthorServiceTests
+    {
+        private readonly BookDbContext _dbContext;
+
+        public AuthorServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<BookDbContext>()
+           .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            _dbContext = new BookDbContext(options);
+        }
+
+        [Theory]
+        [InlineData("Aleksandur", "Simeonov")]
+        [InlineData("Kiril", "Evtimov")]
+        [InlineData("Petur", "Petrov")]
+        public async Task CreateAsyncShouldWorksCorrectly(string firstName, string lastName)
+        {
+            var mockedRepo = new Mock<EfRepository<Author>>(_dbContext); 
+
+            mockedRepo.Setup(x => x.AddAsync(It.IsAny<Author>()))
+                .Callback((Author author) => _dbContext.Authors.AddAsync(author));
+
+            var authorService = new AuthorService(mockedRepo.Object);
+
+            await authorService.CreateAsync(firstName, lastName);
+
+            var actualAuthor = await _dbContext.Authors.FirstOrDefaultAsync();
+
+            _dbContext.Authors.Should().HaveCount(1);
+            actualAuthor.FirstName.Should().BeEquivalentTo(firstName);
+            actualAuthor.LastName.Should().BeEquivalentTo(lastName);
+        }
+
+        [Theory]
+        [InlineData("Aleksandur", "Simeonov")]
+        [InlineData("Kiril", "Evtimov")]
+        [InlineData("Petur", "Petrov")]
+        public async Task GetAuthorNameShouldReturnCorrectValue(string firstName, string lastName)
+        {
+            var mockedRepo = new Mock<EfRepository<Author>>(_dbContext);
+
+            mockedRepo.Setup(x => x.AddAsync(It.IsAny<Author>()))
+                .Callback((Author author) => _dbContext.Authors.Add(author));
+
+            mockedRepo.Setup(x => x.AllAsNoTracking()).Returns(
+                _dbContext.Authors.AsNoTracking());
+
+            var service = new AuthorService(mockedRepo.Object);
+            await service.CreateAsync(firstName, lastName);
+
+            var expected = await service.GetByNameAsync(firstName, lastName);
+
+            expected.FirstName.Should().BeEquivalentTo(firstName);
+            expected.LastName.Should().BeEquivalentTo(lastName);
+        }
+    }
+}
---
+++ Tests/Services/BookServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using BookWebAPI.Data;
+using BookWebAPI.Dtos.Books;
+using BookWebAPI.Models;
+using BookWebAPI.Repositories;
+using BookWebAPI.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace BookWebAPI.Tests.Services
+{
+    public class BookServiceTests
+    {
+        private readonly BookDbContext _dbContext;
+
+        public BookServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<BookDbContext>()
+           .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            _dbContext = new BookDbContext(options);
+        }
+
+        [Fact]
+        public async Task CreateAsyncWorksCorrectly(InputBookDto model)
+        {
+            var bookMockedRepo = new Mock<EfRepository<Book>>(_dbContext);
+            var mapperMocked = new Mock<IMapper>();
+            var userManagerMocked = new Mock<UserManager<ApplicationUser>>();
+            var publisherMockedRepo = new Mock<EfRepository<Publisher>>(_dbContext);
+            var genreMockedRepo = new Mock<EfRepository<Genre>>(_dbContext);
+            var authorMockedRepo = new Mock<EfRepository<Author>>(_dbContext);
+
+
+
+
+            bookMockedRepo.Setup(x => x.AddAsync(It.IsAny<Book>()))
+                .Callback((Book book) => _dbContext.Books.AddAsync(book));
+
+            var publisherService = new PublisherService(publisherMockedRepo.Object);
+            var genreService = new GenreService(genreMockedRepo.Object);
+            var authorService = new AuthorService(authorMockedRepo.Object);
+
+            var booksService = new BookService(bookMockedRepo.Object, mapperMocked.Object,
+                publisherService, genreService, authorService, userManagerMocked.Object);
+
+
+
+        }
+    }
+}
